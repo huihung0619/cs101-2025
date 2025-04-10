d6a755cce12e32a6cf2b6fd71418b639b9f31bf0
huihung0619 <162561891+huihung0619@users.noreply.github.com>
2025-04-10 15:43:38 +0800

    Update main7.c

--- EX#4/main7.c
+++ EX#4/main7.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 
-void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod) {
-    if (n == 1) {
-        printf("\n Move disk 1 from rod %c to rod %c", from_rod, to_rod);
-        return;
-    }
-    towerOfHanoi(n - 1, from_rod, aux_rod, to_rod);
-    printf("\n Move disk %d from rod %c to rod %c", n, from_rod, to_rod);
-    towerOfHanoi(n - 1, aux_rod, to_rod, from_rod);
+int factorial(int n) {
+    if (n == 1) return 1;            // 中止條件（base case）
+    return n * factorial(n - 1);     // 遞迴關係（recursive case）
 }
 
 int main() {
-    int n = 3;
-    towerOfHanoi(n, 'A', 'B', 'C'); // A, B and C are names of rods
+    int n = 5;
+    int m = factorial(n);
+    printf("factorial %d = %d\n", n, m);
     return 0;
 }
